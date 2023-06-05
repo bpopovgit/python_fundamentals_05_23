d18f2fdc7a6fcb45e019ab85017e9f4110901c85
Boris Popov <32306686+bpopovgit@users.noreply.github.com>
2023-06-05 20:59:43 +0300

    add list basics exercises

---
+++ exercises/exercise_lists_basics/invert_values.py
@@ -0,0 +1,9 @@
+new_string = input().split(" ")
+opposite_numbers_list = []
+
+for element in new_string:
+    current_num = -int(element)
+    opposite_numbers_list.append(current_num)
+
+print(opposite_numbers_list)
+
---
+++ exercises/exercise_lists_basics/multiples_list.py
@@ -0,0 +1,8 @@
+factor = int(input())
+count = int(input())
+list_of_nums = []
+
+for multiplier in range(1, count + 1):
+    list_of_nums.append(multiplier * factor)
+
+print(list_of_nums)

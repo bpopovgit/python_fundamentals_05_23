1cc462cf9be27e7fc320a7581288084f080b9457
Boris Popov <32306686+bpopovgit@users.noreply.github.com>
2023-07-07 11:15:21 +0300

    add exercise objects and classes

---
+++ exercises/exercise_objects_and_classes/catalogue.py
@@ -0,0 +1,26 @@
+class Catalogue:
+    products = []
+
+    def __init__(self, name):
+        self.name = name
+
+    def add_product(self, product_name):
+        Catalogue.products.append(product_name)
+
+    def get_by_letter(self, first_letter):
+        return [product for product in self.products if product.startswith(first_letter)]
+
+    def __repr__(self):
+        returned_info = f"Items in the {self.name} catalogue:\n"
+        returned_info += "\n".join(sorted(self.products))
+        return returned_info
+
+
+catalogue = Catalogue("Furniture")
+catalogue.add_product("Sofa")
+catalogue.add_product("Mirror")
+catalogue.add_product("Desk")
+catalogue.add_product("Chair")
+catalogue.add_product("Carpet")
+print(catalogue.get_by_letter("C"))
+print(catalogue)

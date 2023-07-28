60d9b71373408e72b8dbc965982c7d7a7f202a15
Boris Popov <32306686+bpopovgit@users.noreply.github.com>
2023-07-28 23:01:22 +0300

    add regex exercises

---
+++ exercises/exercise_regex/extract_the_links.py
@@ -0,0 +1,13 @@
+import re
+
+valid_urls = []
+pattern = r"(w{3}\.[a-zA-Z0-9-\.]+\.[a-z]+)"
+line = input()
+while line:
+    matches = re.search(pattern, line)
+    if matches:
+        valid_urls.append(matches.group(1))
+    line = input()
+
+for valid_url in valid_urls:
+    print(valid_url)
